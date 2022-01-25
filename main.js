307a342571502238b6ea2d96f34b93137fb03101
lunar0926 <lunar0926@gmail.com>
2022-01-25 23:13:51 +0900

    글목록 출력하기

--- main.js
+++ main.js
@@ -6,14 +6,26 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
-
+  
     if(pathname==='/'){
       // queryData.id가 undefined인 경우
-      if(title===undefined){ 
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      if(queryData.id===undefined){ 
+
+        fs.readdir('../data', function(error, filelist){
+          // 파일 경로는 현재 실행중인 파일을 기준으로 상대적으로 지정해줘야 함. 
+          // 파일 목록을 가져온다음 function을 실행시킴. 
+          console.log(filelist);
           var title = 'Welcome';
           var description = 'This is Node.js';
+          var list = '<ul>';
+          // 반복문으로 filelist 받아오기
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>';
+
           var template = `
           <!doctype html>
           <html>
@@ -23,11 +35,7 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -35,37 +43,51 @@ var app = http.createServer(function(request,response){
           `;
           response.writeHead(200);
           response.end(template);
-        });
+          })
+        
+        
       } else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
+        fs.readdir('../data', function(error, filelist){
+          // 파일 경로는 현재 실행중인 파일을 기준으로 상대적으로 지정해줘야 함. 
+          // 파일 목록을 가져온다음 function을 실행시킴. 
+          var list = '<ul>';
+          // 반복문으로 filelist 받아오기
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>';
+          fs.readFile(`../data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+        }); 
       }
       // 정상적으로 pathname이 / 인 경우
+    
     } else {
       response.writeHead(404);
       response.end('Not found');
-    } 
-    // pathname이 /이 아니고 다른 경우
+    }
+    // pathname이 /이 아니고 다른 경우);
+
+
 });
 app.listen(3000);
